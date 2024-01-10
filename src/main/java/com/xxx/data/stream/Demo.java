3d8fea43260aeb61ea73728fe55660226b48d7f2
yangyalong90@163.com <yangyalong90@163.com>
2024-01-10 20:36:27 +0800

    1

--- src/main/java/com/xxx/data/stream/Demo.java
+++ src/main/java/com/xxx/data/stream/Demo.java
@@ -1,11 +1,13 @@
 package com.xxx.data.stream;
 
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.java.functions.KeySelector;
 import org.apache.flink.api.java.tuple.Tuple;
 import org.apache.flink.api.java.tuple.Tuple2;
-import org.apache.flink.streaming.api.datastream.DataStream;
-import org.apache.flink.streaming.api.datastream.KeyedStream;
-import org.apache.flink.streaming.api.datastream.WindowedStream;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.datastream.*;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.source.SourceFunction;
 import org.apache.flink.streaming.api.functions.timestamps.AscendingTimestampExtractor;
@@ -13,6 +15,7 @@ import org.apache.flink.streaming.api.watermark.Watermark;
 import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
 import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.util.Collector;
 
 import java.util.Random;
 
@@ -25,61 +28,45 @@ public class Demo {
 
     public static void main(String[] args) throws Exception {
         // 创建执行环境
-        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+//        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        Configuration conf = new Configuration();
+        conf.setInteger("rest.port",8081);
+        //显示指定本地运行并开启web页面
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(conf);
 
-        // 创建数据源
-        DataStream<String> stream = env.addSource(new SourceFunction<String>() {
-            private volatile boolean isRunning = true;
-
+        //2. 创建数据流
+        DataStreamSource<String> socket = env.socketTextStream("localhost", 7777);
+        //3. Transform
+        SingleOutputStreamOperator<String> flatMapStream = socket.flatMap(new FlatMapFunction<String, String>() {
             @Override
-            public void run(SourceContext<String> ctx) throws Exception {
-                Random random = new Random();
-                while (isRunning) {
-                    Thread.sleep(10);
-                    long timestamp = System.currentTimeMillis() - random.nextInt(5) * 1000;
-                    String str = "key" + random.nextInt(10) + "," + timestamp;
-                    ctx.collectWithTimestamp(str, timestamp);
-                    ctx.emitWatermark(new Watermark(timestamp));
+            public void flatMap(String s, Collector<String> collector) throws Exception {
+                String[] split = s.split("\\s+");
+                for (String s1 : split) {
+                    collector.collect(s1);
                 }
             }
-
+        });
+        SingleOutputStreamOperator<Tuple2<String, Integer>> mapStream = flatMapStream.map(new MapFunction<String, Tuple2<String, Integer>>() {
             @Override
-            public void cancel() {
-                isRunning = false;
+            public Tuple2<String, Integer> map(String s) throws Exception {
+                return Tuple2.of(s,1);
             }
         });
 
+        KeyedStream<Tuple2<String, Integer>, String> keyByStream = mapStream.keyBy(new KeySelector<Tuple2<String, Integer>, String>() {
+            @Override
+            public String getKey(Tuple2<String, Integer> stringIntegerTuple2) throws Exception {
+                return stringIntegerTuple2.f0;
+            }
+        });
 
-        // 将数据源解析成二元组（key, timestamp）
-        DataStream<Tuple2<String, Long>> parsedStream = stream.map((String line)  -> {
-            String[] parts = line.split(",");
-            return new Tuple2<>((String)parts[0], Long.parseLong(parts[1]));
-        }).returns(Types.TUPLE(Types.STRING, Types.LONG));
+        SingleOutputStreamOperator<Tuple2<String, Integer>> sumStream = keyByStream.sum(1);
 
-        // 设置事件时间和水位线
-        DataStream<Tuple2<String, Long>> withTimestampsAndWatermarks = parsedStream
-                .assignTimestampsAndWatermarks(new AscendingTimestampExtractor<Tuple2<String, Long>>() {
-                    @Override
-                    public long extractAscendingTimestamp(Tuple2<String, Long> element) {
-                        return element.f1;
-                    }
-                });
+        //4. 打印输出
+        sumStream.print();
 
-        // 按键值进行分组
-        KeyedStream<Tuple2<String, Long>, Tuple> keyedStream = withTimestampsAndWatermarks.keyBy(0);
-
-        // 每5秒钟统计最近一分钟的数据（使用滚动时间窗口）
-        WindowedStream<Tuple2<String, Long>, Tuple, TimeWindow> windowedStream = keyedStream.window(TumblingEventTimeWindows.of(Time.minutes(1)));
-
-        // 进行聚合计算
-        DataStream<Tuple2<String, Long>> resultStream = windowedStream
-                .reduce((Tuple2<String, Long> v1, Tuple2<String, Long> v2) -> new Tuple2<>(v1.f0, v1.f1 + v2.f1));
-
-        // 输出结果
-        resultStream.print();
-
-        // 启动作业
-        env.execute("Demo");
+        //5. 提交运行
+        env.execute();
 
     }
 
